9923d8b7ee47524f3d0874a4d68821358c8da5ee
Pavel <ipt.kuzz@gmail.com>
2021-09-20 06:27:04 +0300

    lab with hand written sums

--- lab1/a.cpp
+++ lab1/a.cpp
@@ -1,42 +1,88 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
-
-using namespace cv;
-using namespace std;
-
 #include <vector>
 
+using namespace std;
 
-
-int main(int argc, char** argv)
-{
-    Mat_<u_char> image = imread("cow.jpg", IMREAD_GRAYSCALE);
-
-    if (image.empty()) 
-    {
-        cout << "Could not open or find the image" << endl;
-        cin.get(); 
-        return -1;
+vector<vector<unsigned char>> CreateMatrix() {
+    
+    int n = 0; //rows
+    int m = 0; //cols
+    
+    try {
+        cout << "amount of rows: ";
+        cin >> n;
+        if (n > 1024){
+            throw n;
+        }
+    } catch (int n) {
+        cout << "wrong number of rows: " << n;
     }
 
     try {
-        if (image.rows <= 1024 && image.cols <= 1024) {
-            cout << "image size is ok" << endl;
+        cout << "amount of cols: ";
+        cin >> m;
+        if (m > 1024){
+            throw m;
         }
-        else {
-            throw image;
-        }
-    }
-    catch (Mat matrix) {
-        cout << "image size is incorrect, " << "rows: " << matrix.rows << " columns: " << matrix.cols << endl; 
+    } catch (int m) {
+        cout << "wrong number of cols: " << n;
     }
 
+    vector<vector<unsigned char>> vec( n , vector<unsigned char> (m, 0));
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        for (int j = 0; j < vec[i].size(); j++)
+        {
+            vec[i][j] = (unsigned char) rand();
+        }
+    }
+
+    return vec;
+}
+
+
+unsigned sum(vector<vector<unsigned char>> vec, int x, int y) {
+    unsigned res = 0;
+    for (int i = 0; i <= x; i++)
+    {
+        for (int j = 0; j <= y; j++)
+        {
+            res += vec[i][j];
+        }
+    }
+    return res;
+}
+
+//57449
+
+
+vector<vector<unsigned short>> CreateIntegralImage(vector<vector<unsigned char>> vec) {
+    int n = vec.size();
+    int m = vec[0].size();
+    vector<vector<unsigned short>> II( n , vector<unsigned short> (m, 0));
+
+    int y_counter = 0;
+    int x_counter = 0;
+    while (x_counter != n)
+    {
+        cout << "x_counter: " << x_counter << endl;
+        while (y_counter <= m)
+        {
+            auto full_sum = (unsigned short) (sum(vec,x_counter,y_counter) & 0xFFFF);
+            II[x_counter][y_counter] = full_sum;
+            y_counter += 1;
+        }
+        y_counter = 0;
+        x_counter += 1;
+    }
+
+    return II;
+}
+
+
+unsigned GetRectSum(std::vector<std::vector<unsigned short>> II) {
     
-
-
-    Mat integralImg;
-	integral(image, integralImg, CV_64F);
-
     int x,y,h,w;
     x = y = h = w = 0;
     
@@ -48,7 +94,7 @@ int main(int argc, char** argv)
     try {
         cout << "h: ";
         cin >> h;
-        if (h > 16){
+        if (h > 15){
             throw h;
         }
     }
@@ -60,7 +106,7 @@ int main(int argc, char** argv)
     try {
         cout << "w: ";
         cin >> w;
-        if (w > 16){
+        if (w > 15){
             throw w;
         }
     }
@@ -69,13 +115,47 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    cout << "size of rect is " << h+1 << " x " << w + 1 << endl;
 
-    double p1 = integralImg.at<double>((y), (x));
-	double p2 = integralImg.at<double>((y), (x + w));
-	double p3 = integralImg.at<double>((y + h), (x));
-	double p4 = integralImg.at<double>((y + h), (x + w));
+    unsigned p1 = II[y-1][x-1];
+	unsigned p2 = II[y-1][x + w];
+	unsigned p3 = II[y + h][x-1];
+	unsigned p4 = II[y + h][x + w];
     
-    cout << "sum: " << (p1+p4) - (p2+p3);
+    return (p1+p4) - (p2+p3);
+}
+
+
+int main(int argc, char** argv)
+{
+    
+    auto mat = CreateMatrix();
+    for (int i = 0; i < mat.size(); i++)
+    {
+        for (int j = 0; j < mat[i].size(); j++)
+        {
+            cout << (unsigned)mat[i][j] << "  ";
+        }
+        cout << endl;
+    }
+
+    cout << "---------------------------------" << endl;
+
+    auto II = CreateIntegralImage(mat);
+    
+    
+
+    for (int i = 0; i < II.size(); i++)
+    {
+        for (int j = 0; j < II[i].size(); j++)
+        {
+            cout << II[i][j] << "   ";
+        }
+        cout << endl;
+    }
+
+    unsigned res = GetRectSum(II);
+    cout << res << endl;
 
     return 0;
 }
